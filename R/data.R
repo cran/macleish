ca80b1017118c3f3d535d5af20ccd46c790cc605
Ben Baumer <ben.baumer@gmail.com>
2016-06-09 15:19:17 +0000

    version 0.3.0

---
+++ R/data.R
@@ -0,0 +1,110 @@
+#' Weather data from Macleish Field Stations
+#'
+#' @description Weather data collected at the Macleish Field Station in Whately, 
+#' MA during 2015.
+#'
+#' @details The Macleish Field Station is a remote outpost owned by Smith
+#' College and used for field research. There are two weather stations on the 
+#' premises. One is called \code{WhatelyMet} and the other is \code{OrchardMet}.
+#' 
+#' The \code{WhatelyMet} station is located at (42.448470, -72.680553) and
+#' the \code{OrchardMet} station is at (42.449653, -72.680315).
+#' 
+#' \code{WhatelyMet} is located at the end of Poplar Hill Road in Whately, 
+#' Massachusetts, USA. The meteorological instruments of \code{WhatelyMet} (except the 
+#' rain gauge) are mounted at the top of a tower 25.3 m tall, well above the 
+#' surrounding forest canopy. The tower is located on a local ridge at an 
+#' elevation 250.75m above sea level.
+#' 
+#' \code{OrchardMet} is located about 250 m north of the first tower in an open 
+#' field next to an apple orchard. Full canopy trees (~20 m tall) are within 
+#' 30 m of this station. This station has a standard instrument configuration 
+#' with temperature, relative humidity, solar radiation, and barometric 
+#' pressure measured between 1.5 and 2.0 m above the ground. Wind speed and 
+#' direction are measured on a 10 m tall tower and precipitation is measured 
+#' on the ground. Ground temperature is measured at 15 and 30 cm below the 
+#' ground surface 2 m south of the tower. The tower is located 258.1 m above 
+#' sea level. Data collection at OrchardMet began on June 27th, 2014. 
+#' 
+#' The variables shown above are weather data collected at \code{WhatelyMet} and 
+#' \code{OrchardMet} during 2015. Solar radiation is measured in two different ways: 
+#' see \code{SlrW_Avg}or the \code{PAR} variables for Photosynthetic Active Radiation.
+#' 
+#' Note that a loose wire resulted in erroneous temperature reading at OrchardMet
+#' in late November, 2015. 
+#' 
+#' @docType data
+#' @format For both, a data frame (\code{\link[dplyr]{tbl_df}}) with roughly 52,560 rows and 8 or 9 variables.
+#' 
+#' The following variables are values that are found in either the \code{whately_2015}
+#' or \code{orchard_2015} data tables.
+#' 
+#' All variables are averaged over the 10 minute inverval unless otherwise noted.
+#' \describe{
+#'   \item{when}{Timestamp for each measurment set in Eastern Standard Time.}
+#'   \item{temperature}{average temperature, in Celsius}
+#'   \item{wind_speed}{Wind speed, in meters per second}
+#'   \item{wind_dir}{Wind direction, in degrees}
+#'   \item{rel_humidity}{How much water there is in the air, in millimeters}
+#'   \item{pressure}{Atmospheric pressure, in millibars}
+#'   \item{rainfall}{Total rainfall, in millimeters}
+#'   \item{solar_radiation}{Amount of radiation coming from the sun, in Watts/meters^2. Solar measurement for Whatley}
+#'   \item{par_density}{Photosynthetically Active Radiation (sunlight between 400 and 700 nm), in average density of Watts/meters^2. One of two solar measurements for Orchard}
+#'   \item{par_total}{Photosynthetically Active Radiation (sunlight between 400 and 700 nm), in average total over measurement period of Watts/meters^2. One of two solar measurements for Orchard}
+#' }
+#' @source These data are recorded at \url{http://www.smith.edu/ceeds/macleish_monitoring.php} 
+"whately_2015"
+
+#' @rdname whately_2015
+#' @examples 
+#' 
+#' \dontrun{
+#' #' # loose wire anomalies
+#' if (require(dplyr) & require(ggplot2) & require(lubridate)) {
+#'  orchard_2015 %>%
+#'  filter(month(when) == 11) %>%
+#'    ggplot(aes(x = when, y = temperature)) +
+#'    geom_line() + geom_smooth()
+#' }
+#' }
+"orchard_2015"
+
+#' MacLeish spatial data
+#' 
+#' @description Shapefiles from the MacLeish Field Station. The field station
+#' itself is located at \code{lat = 42.449167}, \code{lon = -72.679389}. These
+#' data contains information about various man-made and natural structures
+#' surrounding the field station. 
+#' 
+#' @details Each of the \code{\link[sp]{Spatial}} objects are projected in 
+#' \code{epsg:4326} for easy integration with \code{\link[ggmap]{ggmap}} or
+#' \code{\link[leaflet]{leaflet}} objects. 
+#' 
+#' @docType data
+#' @format A \code{list} of \code{\link[sp]{Spatial}} objects, each providing a different layer.
+#' 
+#' \describe{
+#'  \item{landmarks}{Landmarks}
+#'  \item{forests}{Type of dominant tree in individual forests, as 
+#'  noted by Jesse Bellemare}
+#'  \item{streams}{local streams}
+#'  \item{challenge_courses}{Challenge courses on the property}
+#'  \item{reservoir}{a reservoir}
+#'  \item{buildings}{Buildings at MacLeish}
+#'  \item{wetlands}{Wetland areas}
+#'  \item{trails}{Hiking trails}
+#'  \item{schools}{Schools in Massachusetts}
+#'  \item{boundary}{the property boundary}
+#'  \item{research}{research plots}
+#'  \item{soil}{soil deposits used by Amy Rhodes}
+#' }
+#' 
+#' @examples 
+#' names(macleish_layers)
+#' summary(macleish_layers[["buildings"]])
+#' 
+#' if (require(sp)) {
+#'  plot(macleish_layers[["buildings"]])
+#' }
+#' 
+"macleish_layers"
---
+++ R/etl_extract.R
@@ -0,0 +1,164 @@
+#' Extract weather data
+#' 
+#' @description Retrieve data from the Macleish Field Station weather monitors
+#' @inheritParams etl::etl_extract
+#' @import etl
+#' @importFrom utils download.file
+#' @export
+#' @examples 
+#' 
+#' macleish <- etl("macleish")
+#' str(macleish)
+#' 
+#' \dontrun{
+#' macleish %>%
+#'   etl_extract() %>%
+#'   etl_transform() %>%
+#'   etl_load()
+#' whately <- macleish %>%
+#'   tbl("whately") 
+#' orchard <- macleish %>%
+#'   tbl("orchard") 
+#'   
+#' whately %>%
+#'   summarize(N = n(), avg_temp = mean(temperature))
+#' orchard %>%
+#'   summarize(N = n(), avg_temp = mean(temperature))
+#'   
+#' # check data types
+#' whately %>%
+#'   glimpse()
+#' 
+#' # if using SQLite, datetimes will get converted to integers
+#' whately <- whately %>%
+#'   mutate(when_datetime = datetime(when, 'unixepoch'))
+#' whately %>%
+#'   glimpse()
+#' 
+#' # show the most recent data -- should be within the past hour
+#' whately %>%
+#'   collect() %>%
+#'   tail()
+#' 
+#' # show that no time-shifting is happening
+#' if (require(ggplot2)) {
+#' macleish %>%
+#'   tbl("whately") %>%
+#'   collect() %>%
+#'   mutate(when = lubridate::ymd_hms(when)) %>%
+#'   filter(lubridate::year(when) == 2012 & month(when) == 12 & day(when) == 20) %>%
+#'   ggplot(aes(x = when, y = temperature)) + geom_line()
+#' }
+#' }
+
+etl_extract.etl_macleish <- function(obj, ...) {
+  url <- "https://scidb.smith.edu/~macleish/"
+  files <- c("WhatelyMet_Met_10min.dat", "OrchardMet_Met_10min.dat")
+  src <- paste0(url, files)
+  lcl <- paste0(attr(obj, "raw_dir"), "/", files)
+  mapply(utils::download.file, src, lcl)
+  invisible(obj)
+}
+
+#' @rdname etl_extract.etl_macleish
+#' @import dplyr
+#' @importFrom lubridate ymd_hms
+#' @importFrom readr read_csv write_csv
+#' @importFrom utils head
+#' @export
+
+
+etl_transform.etl_macleish <- function(obj, ...) {
+  # Whately
+  lcl <- paste0(attr(obj, "raw_dir"), "/WhatelyMet_Met_10min.dat")
+  x <- readr::read_csv(lcl, skip = 1)
+  metadata <- head(x, 2)
+  out <- x[-(1:2), -2]
+  out <- out %>%
+    rename_(when = ~TIMESTAMP) %>%
+    mutate_(when = ~ymd_hms(when)) %>%
+    mutate_(Temp_C_Avg = ~as.numeric(Temp_C_Avg)) %>%
+    rename_(temperature = ~Temp_C_Avg) %>%
+    mutate_(WSpd_mps = ~as.numeric(WSpd_mps)) %>%
+    rename_(wind_speed = ~WSpd_mps) %>%
+    mutate_(Wdir_deg = ~as.numeric(Wdir_deg)) %>%
+    rename_(wind_dir = ~Wdir_deg) %>%
+    mutate_(RH_per_Avg = ~as.numeric(RH_per_Avg)) %>%
+    rename_(rel_humidity = ~RH_per_Avg) %>%
+    mutate_(Press_mb_Avg = ~as.numeric(Press_mb_Avg)) %>%
+    rename_(pressure = ~Press_mb_Avg) %>%
+    mutate_(SlrW_Avg = ~as.numeric(SlrW_Avg)) %>%
+    rename_(solar_radiation = ~SlrW_Avg) %>%
+    mutate_(Rain_mm_Tot = ~as.numeric(Rain_mm_Tot)) %>%
+    rename_(rainfall = ~Rain_mm_Tot) %>%
+    unique()
+  # time-shifting correction, see:
+  # https://github.com/beanumber/macleish/issues/8
+  out <- out %>%
+    mutate_(num = ~seq(1:nrow(out))) %>%
+    mutate_(when = ~ifelse(num <= 50682, when - lubridate::dhours(5), when)) %>%
+    mutate_(when = ~as.POSIXct(when, tz = "EST", origin = "1970-01-01 00:00:00")) %>%
+    select_(~-num)
+  readr::write_csv(out, path = paste0(attr(obj, "load_dir"), "/whately.csv"))
+  
+  # Orchard
+  lcl <- paste0(attr(obj, "raw_dir"), "/OrchardMet_Met_10min.dat")
+  x <- readr::read_csv(lcl, skip = 1)
+  metadata <- head(x, 2)
+  out <- x[-(1:2), -2]
+  out <- out %>%
+    rename_(when = ~TIMESTAMP) %>%
+    mutate_(when = ~ymd_hms(when)) %>%
+    mutate_(Temp_C_Avg = ~as.numeric(Temp_C_Avg)) %>%
+    rename_(temperature = ~Temp_C_Avg) %>%
+    mutate_(WSpd_mps = ~as.numeric(WSpd_mps)) %>%
+    rename_(wind_speed = ~WSpd_mps) %>%
+    mutate_(Wdir_deg = ~as.numeric(Wdir_deg)) %>%
+    rename_(wind_dir = ~Wdir_deg) %>%
+    mutate_(RH_per_Avg = ~as.numeric(RH_per_Avg)) %>%
+    rename_(rel_humidity = ~RH_per_Avg) %>%
+    mutate_(Press_mb_Avg = ~as.numeric(Press_mb_Avg)) %>%
+    rename_(pressure = ~Press_mb_Avg) %>%
+    mutate_(PAR_Den_Avg = ~as.numeric(PAR_Den_Avg)) %>%
+    rename_(par_density = ~PAR_Den_Avg) %>%
+    mutate_(PAR_Tot_Avg = ~as.numeric(PAR_Tot_Avg)) %>%
+    rename_(par_total = ~PAR_Tot_Avg) %>%
+    mutate_(Rain_mm_Tot = ~as.numeric(Rain_mm_Tot)) %>%
+    rename_(rainfall = ~Rain_mm_Tot) %>%
+    unique()
+  # time-shifting correction, see:
+  # https://github.com/beanumber/macleish/issues/8
+  out <- out %>%
+    mutate_(num = ~seq(1:nrow(out))) %>%
+    mutate_(when = ~ifelse( num >= 18482, when + lubridate::dhours(1), when)) %>%
+    mutate_(when = ~ifelse( num >= 70892, when + lubridate::dminutes(50), when)) %>%
+    mutate_(when = ~as.POSIXct(when, tz = "EST", origin = "1970-01-01 00:00:00")) %>%
+    select_(~-num)
+  readr::write_csv(out, path = paste0(attr(obj, "load_dir"), "/orchard.csv"))
+  invisible(obj)
+}
+
+#' @rdname etl_extract.etl_macleish
+#' @importFrom DBI dbWriteTable
+#' @importFrom readr read_csv cols col_double
+#' @export
+
+etl_load.etl_macleish <- function(obj, ...) {
+  # Whately
+  # write the table directly to the DB
+  message("Writing whately data to the database...")
+  if (DBI::dbWriteTable(obj$con, "whately", paste0(attr(obj, "load_dir"), "/whately.csv"), 
+                        overwrite = TRUE, row.names = FALSE, ...)) {
+    message("Data was successfully written to database.")
+    message(DBI::dbListTables(obj$con))
+  }
+  # Orchard
+  # write the table directly to the DB
+  message("Writing orchard data to the database...")
+  if (DBI::dbWriteTable(obj$con, "orchard", paste0(attr(obj, "load_dir"), "/orchard.csv"), 
+                        overwrite = TRUE, row.names = FALSE, ...)) {
+    message("Data was successfully written to database.")
+    message(DBI::dbListTables(obj$con))
+  }
+  invisible(obj)
+}
